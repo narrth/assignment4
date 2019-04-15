f9065b9556579b30c6c174396968878da5fa6b50
Narrthanan Seevananthan <narrthananseevananth@AD.DOE.CARLETON.CA>
2019-04-14 23:52:15 -0400

    first commit

---
+++ assignment4.m
@@ -0,0 +1,15 @@
+%% ELEC 4700 Assignment-4 Circuit Modeling
+% Due: Sunday, Apr. 7, 2019 23:59
+% By: Narrthanan Seevananthan
+
+% Part 1:
+% (a) Formulation:
+
+clear;
+addpath code;
+
+Part1;
+
+% Part 1:
+
+Part2;
